71d85a16fd2a393da7670ff9b060b7c9d9207c5a
shsidulla <shsidullarakib@gmail.com>
2022-02-15 17:09:54 +0600

    new commit

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-document.getElementById('case-plus').addEventListener('click', function name(params) {
-
+function updateCase(updateCase) {
     const caseInput = document.getElementById('case-number');
+    //const caseInput = document.getElementById('case-number');
     const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
+    const casePrice = document.getElementById('total-case-price');
+    const totalCasePrice = casePrice.innerText;
+    if (updateCase == true) {
+        caseInput.value = parseInt(caseNumber) + 1;
+        casePrice.innerText = parseInt(totalCasePrice) + 59;
+    }
+    else if (updateCase == false && caseInput.value > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+        casePrice.innerText = parseInt(totalCasePrice) - 59;
+    }
+}
+
+
+//hand case events
+document.getElementById('case-plus').addEventListener('click', function name(params) {
+    updateCase(true);
+})
+
+document.getElementById('case minus').addEventListener('click', function name(params) {
+
+    updateCase(false);
+
 })
